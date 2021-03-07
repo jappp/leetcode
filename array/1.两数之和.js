a62fc82aab739a7b3d797acb376c2bc492e8fcec
pzr <jappp951@163.com>
2021-03-08 00:51:49 +0800

    两数之和

---
+++ array/1.两数之和.js
@@ -0,0 +1,28 @@
+// 暴力解法
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = nums.length -1; j > i; j--) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+
+/**
+ * 空间换时间
+ * 遍历数组用表存储每一项key为数值, value为下标.
+ * 计算出目标值减去当前值, 并去表中查找,如果查到则返回对应数值
+ */
+var twoSum = function (nums, target) {
+  let map = new Map();
+
+  for (let i = 0, len = nums.length; i < len; i++) {
+    let x = target - nums[i];
+    if (map.has(x)) {
+      return [map.get(x), i];
+    }
+    map.set(nums[i], i);
+  }
+};
